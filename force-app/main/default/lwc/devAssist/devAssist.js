925fe3bf759d1dfa85b1429125c9416ac145bd06
Ellie Rinehart <erinehart@salesforce.com>
2025-04-18 09:55:10 -0400

    Now dynamically picks up Object Context

--- force-app/main/default/lwc/devAssist/devAssist.js
+++ force-app/main/default/lwc/devAssist/devAssist.js
@@ -1,30 +1,40 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, track, wire } from 'lwc';
+import { CurrentPageReference } from 'lightning/navigation';
 import getMergedPRs from '@salesforce/apex/GitHubService.getMergedPRs';
 
 export default class DevAssist extends LightningElement {
-    pullRequests;
+    @track pullRequests;
+    @track fileLinkMap = {};
+    @track inferredPath;
 
-    connectedCallback() {
-        console.log('DevAssist connectedCallback triggered');
-        // Update this path based on the metadata you're displaying contextually
-        const metadataPath = 'force-app/main/default/objects/Contact/fields/My_Second_Field__c.field-meta.xml';
+    @wire(CurrentPageReference)
+    getPageReference(pageRef) {
+        if (pageRef && pageRef.attributes && pageRef.attributes.objectApiName) {
+            const objectName = pageRef.attributes.objectApiName;
+            // Use a placeholder naming pattern for now
+            this.inferredPath = `force-app/main/default/objects/${objectName}/fields/My_Second_Field__c.field-meta.xml`;
+            this.loadPullRequests();
+        }
+    }
 
-        getMergedPRs({ path: metadataPath })
+    loadPullRequests() {
+        if (!this.inferredPath) return;
+
+        getMergedPRs(this.inferredPath)
             .then(data => {
-                this.pullRequests = data.map(pr => ({
-                    ...pr,
-                    fileLinks: pr.files.map(file => ({
-                        name: file,
-                        url: this.buildGitHubFileLink(file)
-                    }))
-                }));
+                this.pullRequests = data;
+                const map = {};
+                data.forEach(pr => {
+                    if (pr.files) {
+                        pr.files.forEach(f => {
+                            map[f] = `https://github.com/erinehart1/sturdy-goggles/blob/main/${f}`;
+                        });
+                    }
+                });
+                this.fileLinkMap = map;
             })
             .catch(error => {
                 console.error('Error fetching PRs', error);
             });
-        }
-        buildGitHubFileLink(filePath) {
-            const repoBase = 'https://github.com/my-org/sturdy-goggles/blob/main/';
-            return `${repoBase}${filePath}`;
-        }
+    }
 }
